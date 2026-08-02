d8a580071f285f2fdca547b003ee8b1a6304ca92
netlify294-coder <netlify294@gmail.com>
2026-08-02 09:01:48 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -349,5 +349,5 @@ async def my_plan(client: Client, message: Message):
             "🔸 Plan: Free\n"
             "🔸 Request: Disabled\n\n"
             "🔓 Unlock Premium to get more benefits\n"
-            "Contact: @GetoPro"
+            "Contact: @"
         )
